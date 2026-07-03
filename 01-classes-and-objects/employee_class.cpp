66462e4925b2bb1059a90a2069448396e2ef5d9d
Prince Sharma <iprincesharma17@gmail.com>
2026-07-03 23:01:04 +0530

    Add Employee class implementation demonstrating object creation

--- 01-classes-and-objects/employee_class.cpp
+++ 01-classes-and-objects/employee_class.cpp
@@ -1,17 +1,26 @@
 /*
-==================================================
-Title   : Employee Class
-Purpose : Demonstrate multiple objects in C++.
-Working :
-1. Create Employee class.
-2. Store employee details.
-3. Create multiple objects.
-4. Display employee information.
-==================================================
+------------------------------------------------------------
+Title       : Employee Class using C++
+Author      : Prince Sharma
+Repository  : oops-using-cpp
+Folder      : 01-classes-and-objects
+File        : 02_employee_class.cpp
+
+Description:
+This program demonstrates the concept of Classes and
+Objects by creating an Employee class.
+
+Concepts Covered:
+- Class
+- Object
+- Private Data Members
+- Public Member Functions
+------------------------------------------------------------
 */
 
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 class Employee
@@ -19,45 +28,44 @@ class Employee
 private:
     int empId;
     string empName;
+    string department;
     float salary;
 
 public:
     void input()
     {
-        cout << "Enter Employee ID: ";
+        cout << "Enter Employee ID : ";
         cin >> empId;
 
         cin.ignore();
 
-        cout << "Enter Employee Name: ";
+        cout << "Enter Employee Name : ";
         getline(cin, empName);
 
-        cout << "Enter Salary: ";
+        cout << "Enter Department : ";
+        getline(cin, department);
+
+        cout << "Enter Salary : ";
         cin >> salary;
     }
 
     void display()
     {
-        cout << "\nEmployee ID   : " << empId << endl;
-        cout << "Employee Name : " << empName << endl;
-        cout << "Salary        : " << salary << endl;
+        cout << "\n========== Employee Details ==========\n";
+        cout << "Employee ID : " << empId << endl;
+        cout << "Name        : " << empName << endl;
+        cout << "Department  : " << department << endl;
+        cout << "Salary      : Rs. " << salary << endl;
     }
 };
 
 int main()
 {
-    Employee e1, e2;
+    Employee emp1;
 
-    cout << "\nEnter Details of Employee 1\n";
-    e1.input();
+    emp1.input();
 
-    cout << "\nEnter Details of Employee 2\n";
-    e2.input();
-
-    cout << "\nEmployee Records\n";
-
-    e1.display();
-    e2.display();
+    emp1.display();
 
     return 0;
 }
