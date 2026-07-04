37e50b7af8d9a6b51433a4de1f1c218199143d00
Prince Sharma <iprincesharma17@gmail.com>
2026-07-04 22:20:28 +0530

    Add Car class implementation demonstrating classes and objects

--- 01-classes-and-objects/car_class.cpp
+++ 01-classes-and-objects/car_class.cpp
@@ -1,53 +1,87 @@
 /*
-==================================================
-Title   : Car Class
-Purpose : Demonstrate class methods.
-Working :
-1. Store car information.
-2. Use member functions.
-3. Display car details.
-==================================================
+------------------------------------------------------------
+Title       : Car Class using C++
+Folder      : 01-classes-and-objects
+File        : 03_car_class.cpp
+
+Purpose:
+To understand the concept of Classes and Objects by
+creating a Car class.
+
+Description:
+This program creates a Car class with private data members
+and public member functions. It accepts car details from
+the user and displays them.
+
+Working:
+1. Create a Car class.
+2. Declare private data members.
+3. Create public member functions.
+4. Accept car details from the user.
+5. Display the entered details.
+
+Concepts Covered:
+- Class
+- Object
+- Private Data Members
+- Public Member Functions
+- Encapsulation (Basic)
+------------------------------------------------------------
 */
 
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 class Car
 {
 private:
+    int carId;
     string brand;
     string model;
     int year;
+    float price;
 
 public:
-    void setData()
+    void input()
     {
-        cout << "Enter Brand: ";
-        cin >> brand;
+        cout << "Enter Car ID : ";
+        cin >> carId;
 
-        cout << "Enter Model: ";
-        cin >> model;
+        cin.ignore();
 
-        cout << "Enter Manufacturing Year: ";
+        cout << "Enter Brand : ";
+        getline(cin, brand);
+
+        cout << "Enter Model : ";
+        getline(cin, model);
+
+        cout << "Enter Manufacturing Year : ";
         cin >> year;
+
+        cout << "Enter Price : ";
+        cin >> price;
     }
 
-    void showData()
+    void display()
     {
-        cout << "\n----- Car Details -----\n";
-        cout << "Brand : " << brand << endl;
-        cout << "Model : " << model << endl;
-        cout << "Year  : " << year << endl;
+        cout << "\n========== Car Details ==========\n";
+        cout << "Car ID      : " << carId << endl;
+        cout << "Brand       : " << brand << endl;
+        cout << "Model       : " << model << endl;
+        cout << "Year        : " << year << endl;
+        cout << "Price       : Rs. " << price << endl;
     }
 };
 
 int main()
 {
-    Car c1;
+    Car car1;
 
-    c1.setData();
-    c1.showData();
+    car1.input();
+
+    car1.display();
 
     return 0;
 }
