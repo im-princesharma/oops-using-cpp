ebb37bf876d849302cf7a457229a2574de81d131
Prince Sharma <iprincesharma17@gmail.com>
2026-07-06 22:37:02 +0530

    Clarify purpose in array_of_objects.cpp
    Updated purpose section for clarity.

--- 01-classes-and-objects/array_of_objects.cpp
+++ 01-classes-and-objects/array_of_objects.cpp
@@ -4,7 +4,7 @@ Title       : Array of Objects using C++
 Folder      : 01-classes-and-objects
 File        : array_of_objects.cpp 
       
-Purpose:      
+Purpose:       
 To understand how to create and use an array of          
 objects in C++.    
 
