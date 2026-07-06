2d92d512b59fb81136dfc9d61bcc69806c43038b
Prince Sharma <iprincesharma17@gmail.com>
2026-07-06 22:28:38 +0530

    Update array_of_objects.cpp

--- 01-classes-and-objects/array_of_objects.cpp
+++ 01-classes-and-objects/array_of_objects.cpp
@@ -4,9 +4,9 @@ Title       : Array of Objects using C++
 Folder      : 01-classes-and-objects
 File        : array_of_objects.cpp 
 
-Purpose: 
+Purpose:  
 To understand how to create and use an array of 
-objects in C++.
+objects in C++. 
 
 Description:
 This program creates an array of Student objects,
