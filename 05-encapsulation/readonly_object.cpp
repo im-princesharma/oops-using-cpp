21a6b6c15378b73515ee9ec6804c2470f989d50e
Prince Sharma <iprincesharma17@gmail.com>
2026-07-08 12:06:40 +0530

    Implement read-only object using const member function

---
+++ 05-encapsulation/readonly_object.cpp
@@ -0,0 +1,67 @@
+/*
+------------------------------------------------------------
+Title       : Read-Only Object using Const Member Function
+Folder      : 05-encapsulation
+File        : readonly_object.cpp
+
+Purpose:
+To understand how const member functions allow
+read-only access to object data.
+
+Description:
+This program demonstrates how const member
+functions can be used to safely access data
+without modifying the object's state.
+
+Working:
+1. Create a Book class.
+2. Store book details privately.
+3. Display details using a const member function.
+4. Access the object as a const object.
+
+Concepts Covered:
+- Encapsulation
+- Const Member Function
+- Read-Only Object
+- Data Protection
+------------------------------------------------------------
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Book
+{
+private:
+    int bookId;
+    string title;
+    string author;
+
+public:
+
+    Book(int id, string t, string a)
+    {
+        bookId = id;
+        title = t;
+        author = a;
+    }
+
+    void display() const
+    {
+        cout << "\n===== Book Details =====" << endl;
+        cout << "Book ID : " << bookId << endl;
+        cout << "Title   : " << title << endl;
+        cout << "Author  : " << author << endl;
+    }
+};
+
+int main()
+{
+    const Book book(101, "Object Oriented Programming", "Bjarne Stroustrup");
+
+    book.display();
+
+    return 0;
+}
