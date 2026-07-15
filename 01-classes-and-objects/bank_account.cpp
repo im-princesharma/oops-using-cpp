b55b945ced04b294b037b9fee4bf1254a3882acf
Prince Sharma <iprincesharma17@gmail.com>
2026-07-15 16:36:12 +0530

    Enhance comments in bank_account.cpp
    Added purpose and description comments for the BankAccount class.

--- 01-classes-and-objects/bank_account.cpp
+++ 01-classes-and-objects/bank_account.cpp
@@ -7,7 +7,7 @@ File        : bank_account.cpp
 Purpose:
 To understand how member functions can perform   
 banking operations such as deposit and withdrawal. 
-
+ 
 Description: 
 This program creates a BankAccount class that stores    
 account information. It allows the user to deposit   
