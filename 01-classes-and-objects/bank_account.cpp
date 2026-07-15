6675183e40a13e554f81a42f53fb19bfe967c8d9
Prince Sharma <iprincesharma17@gmail.com>
2026-07-15 16:36:48 +0530

    Revise purpose section in bank_account.cpp
    Updated the purpose section of the bank account class file.

--- 01-classes-and-objects/bank_account.cpp
+++ 01-classes-and-objects/bank_account.cpp
@@ -4,7 +4,7 @@ Title       : Bank Account Class using C++
 Folder      : 01-classes-and-objects   
 File        : bank_account.cpp 
  
-Purpose:
+Purpose: 
 To understand how member functions can perform    
 banking operations such as deposit and withdrawal. 
  
