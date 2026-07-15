a76cbe6a6f7159427db9cb6fad6910925410fc94
Prince Sharma <iprincesharma17@gmail.com>
2026-07-15 16:37:00 +0530

    Update comments in bank_account.cpp

--- 01-classes-and-objects/bank_account.cpp
+++ 01-classes-and-objects/bank_account.cpp
@@ -18,7 +18,7 @@ Working:
 2. Accept account details.   
 3. Display a menu.
 4. Perform Deposit or Withdraw operation. 
-5. Display updated account information.
+5. Display updated account information. 
 
 Concepts Covered:
 - Class
